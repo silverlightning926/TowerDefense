75e75c313179256b43db2661b8b7db70aef030af
Siddharth Rao <38925407+silverlightning926@users.noreply.github.com>
2018-11-12 00:05:08 -0800

    Added Missile Launcher
    Added Missile Launcher to the game and replaced all instances of Another Turret to Missile Launcher.

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -15,9 +15,9 @@ public class Shop : MonoBehaviour {
         buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
     }
 
-    public void PurchaseAnotherTurret ()
+    public void PurchaseMissileLauncher ()
     {
-        Debug.Log("Another Standard Turret Selected");
-        buildManager.SetTurretToBuild(buildManager.anotherTurretPrefab);
+        Debug.Log("Missile Launcher Selected");
+        buildManager.SetTurretToBuild(buildManager.missileLauncherPrefab);
     }
 }
