a027cee70c93d4751ad95f95146f001f914bffb1
Siddharth Rao <38925407+silverlightning926@users.noreply.github.com>
2018-11-12 21:46:23 -0800

    Updated GUI
    Added text to display the amount of money the player has. Moved the wave counter to the bottom right. Added price tags for the turrets

---
+++ Assets/Scripts/MoneyUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MoneyUI : MonoBehaviour {
+
+    public Text moneyText;
+
+	void Update () {
+		
+        moneyText.text  = "$" + PlayerStats.Money.ToString();
+	}
+}
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -26,7 +26,10 @@ public class WaveSpawner : MonoBehaviour {
         }
 
         countdown -= Time.deltaTime;
-        waveCountDownText.text = Mathf.Round(countdown).ToString();
+
+        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+
+        waveCountDownText.text = string.Format("{0:00.00}", countdown);
     }
 
     IEnumerator SpawnWave()
