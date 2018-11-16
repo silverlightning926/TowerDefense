21d1e886e39911594ee5b2c9bbf6a41764077636
Siddharth Rao <38925407+silverlightning926@users.noreply.github.com>
2018-11-16 08:33:35 -0800

    Added a Node UI
    Created UI for selling and upgrading turrets.

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -3,6 +3,10 @@
 public class BuildManager : MonoBehaviour {
 
     public static BuildManager instance;
+    public NodeUI nodeUI;
+
+    private Node selectedNode;
+
 
     void Awake()
     {
@@ -59,5 +63,27 @@ public class BuildManager : MonoBehaviour {
     public void SelectTurretToBuild(TurretBlueprint turret)
     {
         turretToBuild = turret;
+
+        DeselectNode();
+    }
+
+    public void SelectNode(Node node)
+    {
+        if (selectedNode == node)
+        {
+            DeselectNode();
+            return;
+        }
+
+        selectedNode = node;
+        turretToBuild = null;
+
+        nodeUI.SetTarget(node); 
+    }
+
+    public void DeselectNode()
+    {
+        selectedNode = null;
+        nodeUI.Hide();
     }
 }
