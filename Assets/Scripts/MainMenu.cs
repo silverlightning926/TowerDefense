8c2418e72eec29b508d9847e50ae790094ecbd9b
Siddharth Rao <38925407+silverlightning926@users.noreply.github.com>
2018-11-27 21:09:48 -0800

    Added A Main Menu
    Created a main menu with a play and quit button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public string LevelToLoad = "MainLevel";
+
+    public void Play()
+    {
+        Debug.Log("Play!");
+        SceneManager.LoadScene(LevelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
