f8eddd826a072a179207defb43383ddb0bcf2a43
Siddharth Rao <38925407+silverlightning926@users.noreply.github.com>
2018-12-03 14:19:16 -0800

    Added Health Bar To Enemy
    The enemy now projects a healthbar which displays the health of the enemy beneath it.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
     public float startSpeed = 10f;
+    public float startHealth = 100;
+
+    [Header("Unity Stuff")]
+
+    public Image healthBar;
 
     [HideInInspector]
     public float speed = 10f;
 
-    public float health = 100;
+    private float health;
 
     public int reward = 25;
 
@@ -16,11 +22,14 @@ public class Enemy : MonoBehaviour {
     void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if (health < 0)
         {
